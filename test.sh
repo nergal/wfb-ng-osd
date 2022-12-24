dda68525385a7b58df8e6d7140ce2552b8d6579d
Artem Poliukhovych <nergal@users.noreply.github.com>
2022-12-25 01:43:12 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 gst-launch-1.0 rtspsrc latency=5 location="rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4" ! rtph264depay ! h264parse ! avdec_h264 ! videoscale method=0 ! video/x-raw,width=1920,height=1080 ! videoconvert ! cairooverlay ! videoconvert ! fpsdisplaysink video-sink=autovideosink sync=false
+
+gst-launch-1.0 videotestsrc ! cairooverlay name=overlay ! fpsdisplaysink video-sink=autovideosink sync=false # 60 fps
+gst-launch-1.0 videotestsrc ! videoscale method=0 ! video/x-raw,width=1920,height=1080 ! videoconvert ! cairooverlay name=overlay ! videoconvert ! fpsdisplaysink video-sink=autovideosink sync=false # 6 fps
